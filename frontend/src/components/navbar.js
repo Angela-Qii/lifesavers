db064a3f85a9a4ddb99befe196c3933a144b6923
Angela-Qii <angelaqi003@gmail.com>
2025-05-16 03:11:06 -0700

    added top and nav bar css

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import './style/navbar.css';
+
 
 function NavBar() {
 
--- frontend/src/components/topbar.js
+++ frontend/src/components/topbar.js
@@ -1,19 +1,20 @@
-function TopBar({user}) {
+import './style/topbar.css';
 
-return (
-  <nav class="topbar">
-    <ul>
-      <li class="lightblue_bg">
-        <a href="/"><img src="imgs/logo.png" alt="Circular FitCheck logo"/></a>
-        <a href="/">FitCheck</a>
-      </li>
-      <li>
-        {user ? <a href="/logout">Sign Out</a> : null}
-        <a href="/account"><img src="imgs/person.png" alt="Person icon"/></a>
-        <a href="/account"> {user ? "Hello " + user.displayName + "!" : "Sign In"}</a>
-      </li>
-    </ul>
-  </nav>
+function TopBar({ user }) {
+  return (
+    <nav className="topbar">
+      <ul>
+        <li className="left-section">
+          <a href="/"><img src="imgs/logo.png" alt="Circular FitCheck logo" /></a>
+          <a href="/">FitCheck</a>
+        </li>
+        <li>
+          {user ? <a href="/logout">Sign Out</a> : null}
+          <a href="/account"><img src="imgs/person.png" alt="Person icon" /></a>
+          <a href="/account">{user ? `Hello ${user.displayName}!` : 'Sign In'}</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
